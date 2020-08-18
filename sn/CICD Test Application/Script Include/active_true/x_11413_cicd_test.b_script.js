091b2c62cc6d4289fb492ec04aedea21999aa0e5
System Administrator <boris_moers@rcomext.com>
2020-08-18 14:26:15 +0200

    Changes by 'System Administrator' on 'CICD Test Application - 2 A' -- Build #1

---
+++ sn/CICD Test Application/Script Include/active_true/x_11413_cicd_test.b_script.js
@@ -0,0 +1,16 @@
+/* 
+ * Application : CICD Test Application
+ * ClassName   : sys_script_include
+ * Created On  : 2020-08-18 12:24:45
+ * Created By  : admin
+ * Updated On  : 2020-08-18 12:24:45
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=c0f96240db765490fcf41780399619dd
+ */
+var b_script = Class.create();
+b_script.prototype = {
+    initialize: function() {
+    },
+
+    type: 'b_script'
+};
