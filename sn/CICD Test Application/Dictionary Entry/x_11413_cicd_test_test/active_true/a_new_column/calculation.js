bf20143b5ab6ad5b095ed1ada0d8d5a7cd09c2e6
bmoers <boris@moers.ch>
2020-09-10 11:21:09 +0200

    'master' branch updated from  https://dev54289.service-now.com no-cicd

--- sn/CICD Test Application/Dictionary Entry/x_11413_cicd_test_test/active_true/a_new_column/calculation.js
+++ sn/CICD Test Application/Dictionary Entry/x_11413_cicd_test_test/active_true/a_new_column/calculation.js
@@ -3,7 +3,7 @@
  * ClassName   : sys_dictionary
  * Created On  : 2019-09-19 12:31:44
  * Created By  : admin
- * Updated On  : 2019-09-19 12:31:44
+ * Updated On  : 2020-09-10 09:16:02
  * Updated By  : admin
  * URL         : /sys_dictionary.do?sys_id=e7786268db444c10fcf4178039961965
  */
--- sn/CICD Test Application/Dictionary Entry/x_11413_cicd_test_test/active_true/collection/calculation.js
+++ sn/CICD Test Application/Dictionary Entry/x_11413_cicd_test_test/active_true/collection/calculation.js
@@ -3,7 +3,7 @@
  * ClassName   : sys_dictionary
  * Created On  : 2019-08-30 11:03:17
  * Created By  : admin
- * Updated On  : 2019-08-30 11:03:17
+ * Updated On  : 2020-09-10 09:16:02
  * Updated By  : admin
  * URL         : /sys_dictionary.do?sys_id=d3749342db63ff04fcf41780399619ba
  */
